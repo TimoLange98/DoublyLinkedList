821bdde1f4ce83ae17f9eb43f7016d349da7e72f
Timo Lange <lange@landaumedia.de>
2020-02-05 17:40:57 +0100

    Erster Commit

---
+++ DoublyLinkedList/List.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    public class List<T>
+    {
+        public Node<T> First;
+        public Node<T> Last;
+
+        public void Add(T data)
+        {
+            if (First == null)
+            {
+                var add = new Node<T>(null, data, null);
+                First = add;
+                Last = add;
+            }
+            else
+            {
+                var add = new Node<T>(Last, data, null);
+                Last.Next = add;
+                Last = add;
+            }
+        }
+
+
+        public void AddRange(List<T> add)
+        {
+            var help = add.First;
+
+            for (int i = 0; i < add.Length(); i++)
+            {
+                Add(help.Data);
+                help = help.Next;
+            }
+        }
+
+
+        public void Clear()
+        {
+            First = Last = null;
+        }
+
+        
+        public void PrintToConsole()
+        {
+            var help = First;
+
+            
+            while (help != null)
+            {
+                if (help.Next == null)
+                {
+                    Console.Write("|" + help.Data + "|\n");
+                    break;
+                }
+                else
+                {
+                    Console.Write("|" + help.Data + "|->");
+                    help = help.Next;
+                }
+            }
+        }
+
+
+        private int Length()
+        {
+            var help = First;
+            var result = 0;
+
+            while (help != null)
+            {
+                result++;
+                help = help.Next;
+            }
+            return result;
+        }
+
+
+        public void Remove(T value)
+        {
+            var help = First;
+
+            while (!help.Data.Equals(value))
+            {
+                help = help.Next;
+            }
+
+            var before = help.Prev;
+
+            before.Next = help.Next;
+            help.Next.Prev = before;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Node<T> help = First;
+
+            if (index == 0)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+
+        public void Reverse()
+        {
+            var help = Last;
+            help.SwitchReferences();
+            First = help;
+            Last = help;
+
+            while (help.Next != null)
+            {
+                help = help.Next;
+                help.SwitchReferences();
+                Last.Next = help;
+                Last = Last.Next;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[Length()];
+            var help = First;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = help.Data;
+                help = help.Next;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myListInt = new List<int>();
+
+            myListInt.Add(1);
+            myListInt.Add(2);
+            myListInt.Add(3);
+            myListInt.Add(4);
+            myListInt.Add(5);
+
+            List<int> myListInt2 = new List<int>();
+
+            myListInt2.Add(6);
+            myListInt2.Add(7);
+            myListInt2.Add(8);
+            myListInt2.Add(9);
+
+            myListInt.AddRange(myListInt2);
+            myListInt.Reverse();
+            myListInt.Remove(7);
+
+            myListInt.PrintToConsole();
+        }
+    }
+}
